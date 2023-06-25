d5c4c172f0487f0136d6bd1c6d883e5e34f46887
ttwange <ttwange2@gmail.com>
2023-06-25 16:02:41 +0300

    Working on passgen app

--- passgen/views.py
+++ passgen/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Hellos there its me!!')
+    return HttpResponse('<h1>Hellos there its me!!</h1> <br> <h2>Thone</h2>')
+
+def passgen(request):
+    char = list('abcdefghijklmnopqrstuvwyz')
